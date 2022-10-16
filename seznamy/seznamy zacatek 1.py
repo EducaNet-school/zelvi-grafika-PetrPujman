e56a84a6d723e572d1fdb34c44732a0e3e678d32
12-pp <113425694+12-pp@users.noreply.github.com>
2022-10-16 19:27:46 +0200

    poznamky

--- seznamy/seznamy zacatek 1.py
+++ seznamy/seznamy zacatek 1.py
@@ -2,7 +2,7 @@ list = []
 list2 = []
 pocet = int(input("kolik prvocisel?"))
 for cislo in range(1, 1000):
-    if cislo > 1:
+    if cislo > 1: #zacni range od 2 a muzes vyhodit podminku
         for i in range(2, cislo):
             if (cislo % i) == 0:
                 break
@@ -13,3 +13,5 @@ for i in range(pocet):
     list2.append(list[i])
 
 print(list2)
+# je to dobre, ale zbutecne presypas seznamy, mrkni na poznamku na radku 5
+
--- seznamy/seznamy zacatek 2.py
+++ seznamy/seznamy zacatek 2.py
@@ -7,3 +7,4 @@ for i in range(1, 10):
     seznam.append(x)
 
 print(seznam)
+#cisel je jen 9
